5e5b0d433eea23bffc0dda8d976a8d3fd8f56f7b
JoeyHammoth <137293223+JoeyHammoth@users.noreply.github.com>
2025-02-17 18:39:35 +1100

    Implemented full calculator functionality

--- function.js
+++ function.js
@@ -1,4 +1,6 @@
 var num = 0;
+var result = 0;
+var currOp = 0;
 const screen = document.querySelector(".screen");
 const clear = document.querySelector("#but-c");
 const back = document.querySelector("#but-back");
@@ -11,7 +13,71 @@ const nums = document.querySelectorAll(".num"); // getting all elements with cla
 
 clear.addEventListener("click", function () {
     num = 0;
+    result = 0;
     screen.innerText = num;
+});
+
+back.addEventListener("click", function () {
+    num /= 10;
+    num = Math.floor(num);
+    screen.innerText = num;
+});
+
+add.addEventListener("click", function () {
+    if (currOp == 0) {
+        result = num;
+    } else {
+        result += num;
+    }
+    num = 0;
+    currOp = 1;
+});
+
+minus.addEventListener("click", function () {
+    if (currOp == 0) {
+        result = num;
+    } else {
+        result -= num;
+    }
+    num = 0;
+    currOp = 2;
+});
+
+prod.addEventListener("click", function () {
+    if (currOp == 0) {
+        result = num;
+    } else {
+        result *= num;
+    }
+    num = 0;
+    currOp = 3;
+});
+
+div.addEventListener("click", function () {
+    if (currOp == 0) {
+        result = num;
+    } else {
+        result /= num;
+        result = Math.floor(result);
+    }
+    num = 0;
+    currOp = 4;
+});
+
+equal.addEventListener("click", function () {
+    if (currOp == 1) {
+        result += num;
+    } else if (currOp == 2) {
+        result -= num;
+    } else if (currOp == 3) {
+        result *= num;
+    } else if (currOp == 4) {
+        result /= num;
+    }
+    screen.innerText = result;
+    currOp = 0;
+    num = 0;
+    result = 0;
 });
 
 nums.forEach(function (button) { // iterating through all elements part of class num
